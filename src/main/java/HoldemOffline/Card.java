acce0b3a7da940a8916f12003e4e4d5cfd8d5e16
DESKTOP-JT6G81S\Mikołaj <mikolaj.twarog@student.uj.edu.pl>
2018-04-22 22:32:55 +0200

    updating Card

--- src/main/java/HoldemOffline/Card.java
+++ src/main/java/HoldemOffline/Card.java
@@ -5,13 +5,24 @@ public class Card {
         TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN,
         JACK, QUEEN, KING, ACE
     }
+
     public enum Suit{
         HEART, DIAMOND, CLUB, SPADE
     }
-    Rank rank;
-    Suit suit;
+
+    private final Rank rank;
+    private final Suit suit;
+
     public Card(Rank rank, Suit suit){
         this.rank=rank;
         this.suit=suit;
+    }
+
+    public Rank getRank(){
+        return rank;
+    }
+
+    public Suit getSuit(){
+        return suit;
     }
 }
