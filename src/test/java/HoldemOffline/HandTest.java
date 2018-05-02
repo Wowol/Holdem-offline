d4b5dad94e7af4c31a60e9e02c7e1e854bb88060
DESKTOP-JT6G81S\Mikołaj <mikolaj.twarog@student.uj.edu.pl>
2018-05-02 13:17:09 +0200

    HandTest

---
+++ src/test/java/HoldemOffline/HandTest.java
@@ -0,0 +1,30 @@
+package HoldemOffline;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+public class HandTest {
+    @Test
+    public void test1(){
+        ArrayList<Card> cards = new ArrayList<>();
+        cards.add(new Card(Rank.NINE, Suit.HEART));
+        cards.add(new Card(Rank.NINE, Suit.SPADE));
+        ArrayList<Card> kickers = new ArrayList<>();
+        kickers.add(new Card(Rank.TEN, Suit.SPADE));
+        kickers.add(new Card(Rank.KING, Suit.SPADE));
+        kickers.add(new Card(Rank.QUEEN, Suit.DIAMOND));
+        Hand a = new Hand(HandName.ONE_PAIR, cards, kickers);
+        ArrayList<Card> cards2 = new ArrayList<>();
+        cards2.add(new Card(Rank.NINE, Suit.HEART));
+        cards2.add(new Card(Rank.NINE, Suit.SPADE));
+        ArrayList<Card> kickers2 = new ArrayList<>();
+        kickers2.add(new Card(Rank.TWO, Suit.SPADE));
+        kickers2.add(new Card(Rank.ACE, Suit.SPADE));
+        kickers2.add(new Card(Rank.QUEEN, Suit.DIAMOND));
+        Hand b = new Hand(HandName.ONE_PAIR, cards2, kickers2);
+        assertEquals(a.compareTo(b), -1);
+    }
+}
