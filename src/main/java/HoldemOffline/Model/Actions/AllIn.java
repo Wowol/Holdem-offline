df4ac9cd4953c17557eba95ad42c135112e23ad6
DESKTOP-JT6G81S\Mikołaj <mikolaj.twarog@student.uj.edu.pl>
2018-06-05 16:10:04 +0200

    Allin update, some tests

--- src/main/java/HoldemOffline/Model/Actions/AllIn.java
+++ src/main/java/HoldemOffline/Model/Actions/AllIn.java
@@ -5,76 +5,52 @@ import HoldemOffline.Model.Player;
 import HoldemOffline.Model.Pot;
 import HoldemOffline.Model.Actions.Exceptions.ActionException;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class AllIn extends Action {
 
     @Override
     protected void make(Player player) throws ActionException {
-        Pot maxPot = new Pot();
+        Pot newPot = null;
 
-        for (Pot p : player.table.currentTurnPots) {
-            if (p.maxBet > maxPot.maxBet) {
-                maxPot = p;
+        List<Pot> temp = new ArrayList<>(player.table.allPots);
+
+        for (Pot p : player.table.allPots) {
+            if (p.players.get(player) != null) {
+                player.numberOfChips += p.players.get(player);
+                p.chips -= p.players.get(player);
+                p.players.remove(player);
             }
         }
 
-        Pot newPot = new Pot();
+        while (!temp.isEmpty()) {
+            Pot minPot = null;
 
-        if (maxPot.maxBet > player.numberOfChips) {
-            for (Map.Entry<Player, Integer> p : maxPot.players.entrySet()) {
-                if (p.getValue() >= player.numberOfChips) {
-                    maxPot.chips -= player.numberOfChips;
-                    p.setValue(p.getValue() - player.numberOfChips);
-                    newPot.players.put(p.getKey(), player.numberOfChips);
-                    newPot.chips += player.numberOfChips;
-                } else {
-                    maxPot.chips -= p.getValue();
-                    newPot.players.put(p.getKey(), p.getValue());
-                    newPot.chips += p.getValue();
-                    p.setValue(0);
+            for (Pot p : temp) {
+                if (minPot == null || minPot.maxBet > p.maxBet) {
+                    minPot = p;
                 }
             }
 
-            newPot.maxBet = player.numberOfChips;
-            newPot.players.putIfAbsent(player, player.numberOfChips);
-            newPot.chips += player.numberOfChips;
+            if (minPot == player.table.mainPot) {
+                temp.remove(minPot);
+                continue;
+            }
 
-            maxPot.maxBet -= player.numberOfChips;
+            if (minPot.maxBet > player.numberOfChips) {
+                newPot = new Pot();
 
-        } else {
-            if (player.numberOfChips > player.numberOfChipsNeededToCall()) {
-                newPot.maxBet = player.numberOfChips - player.numberOfChipsNeededToCall();
-                newPot.chips = newPot.maxBet;
-
-                for (Pot p : player.table.currentTurnPots) {
-                    p.players.putIfAbsent(player, 0);
-                    p.chips += p.maxBet - p.players.get(player);
-                    p.players.put(player, p.maxBet);
-                }
-
-                newPot.players.put(player, newPot.maxBet);
-            } else {
-                newPot.maxBet = player.numberOfChips;
-
-                for (Pot p : player.table.currentTurnPots) {
-                    if (p != maxPot) {
-                        p.players.putIfAbsent(player, 0);
-                        p.chips += p.maxBet - p.players.get(player);
-                        player.numberOfChips -= p.maxBet - p.players.get(player);
-                        newPot.maxBet -= p.maxBet - p.players.get(player);
-                        p.players.put(player, p.maxBet);
-                    }
-                }
-
-                for (Map.Entry<Player, Integer> p : maxPot.players.entrySet()) {
+                for (Map.Entry<Player, Integer> p : minPot.players.entrySet()) {
                     if (p.getValue() >= player.numberOfChips) {
-                        maxPot.chips -= player.numberOfChips;
+                        minPot.chips -= player.numberOfChips;
                         p.setValue(p.getValue() - player.numberOfChips);
                         newPot.players.put(p.getKey(), player.numberOfChips);
                         newPot.chips += player.numberOfChips;
                     } else {
-                        maxPot.chips -= p.getValue();
+                        minPot.chips -= p.getValue();
                         newPot.players.put(p.getKey(), p.getValue());
                         newPot.chips += p.getValue();
                         p.setValue(0);
@@ -82,25 +58,57 @@ public class AllIn extends Action {
                 }
 
                 newPot.maxBet = player.numberOfChips;
-                newPot.players.putIfAbsent(player, player.numberOfChips);
+                newPot.players.put(player, player.numberOfChips);
                 newPot.chips += player.numberOfChips;
+                minPot.maxBet -= player.numberOfChips;
 
-                maxPot.maxBet -= player.numberOfChips;
+                player.table.currentTurnPots.add(newPot);
+                player.table.allPots.add(newPot);
+
+                break;
+            } else {
+                minPot.players.put(player, minPot.maxBet);
+                minPot.chips += minPot.maxBet;
+                player.numberOfChips -= minPot.maxBet;
+                temp.remove(minPot);
             }
+
         }
 
-        if (player.numberOfChips > player.table.maxBetInCurrentTurn) {
-            player.table.maxBetInCurrentTurn = player.numberOfChips;
-            for (Player p : player.table.players) {
-                if(p.isPlaying)
-                    p.isPlayingThisTurn = true;
+        if (newPot == null) {
+            newPot = new Pot();
+
+            if (player.table.mainPot.maxBet < player.numberOfChips) {
+                newPot = player.table.mainPot;
+                player.table.mainPot = new Pot();
+                player.table.currentTurnPots.add(player.table.mainPot);
+                player.table.allPots.add(player.table.mainPot);
+                player.table.maxBetInCurrentTurn = player.numberOfChips;
+            } else {
+                for (Map.Entry<Player, Integer> p : player.table.mainPot.players.entrySet()) {
+                    if (p.getValue() >= player.numberOfChips) {
+                        player.table.mainPot.chips -= player.numberOfChips;
+                        p.setValue(p.getValue() - player.numberOfChips);
+                        newPot.players.put(p.getKey(), player.numberOfChips);
+                        newPot.chips += player.numberOfChips;
+                    } else {
+                        player.table.mainPot.chips -= p.getValue();
+                        newPot.players.put(p.getKey(), p.getValue());
+                        newPot.chips += p.getValue();
+                        p.setValue(0);
+                    }
+                }
+                player.table.mainPot.maxBet -= player.numberOfChips;
+                player.table.currentTurnPots.add(newPot);
+                player.table.allPots.add(newPot);
             }
+
+            newPot.maxBet = player.numberOfChips;
+            newPot.players.put(player, player.numberOfChips);
+            newPot.chips += player.numberOfChips;
         }
 
         player.numberOfChips = 0;
-
-        player.table.currentTurnPots.add(newPot);
-        player.table.allPots.add(newPot);
 
         player.isPlaying = false;
         player.isAllIn = true;
---
+++ src/test/java/HoldemOffline/Model/Games/TestFunctions.java
@@ -0,0 +1,21 @@
+package HoldemOffline.Model.Games;
+
+import HoldemOffline.Model.Pot;
+import HoldemOffline.Model.Table;
+
+import java.util.Map;
+
+public class TestFunctions {
+    public static String printAllPots (Table table) {
+        StringBuilder wyn = new StringBuilder();
+        for (Pot p : table.allPots) {
+            wyn.append("MAXBET: " + p.maxBet + "\n");
+            wyn.append("CHIPS: " + p.chips + "\n");
+            for (Map.Entry pl : p.players.entrySet()) {
+                wyn.append("player " + (table.players.indexOf(pl.getKey())+1) + ": " + pl.getValue() + "\n");
+            }
+            wyn.append("\n");
+        }
+        return wyn.toString();
+    }
+}
